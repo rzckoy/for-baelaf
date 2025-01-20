4d9e7ea248f6081e905fdb57ae092cdef1769536
rzckoy <alxndrrichard2112@gmail.com>
2025-01-20 18:00:00 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I love you, Baelaf.'.split('')
+const text = 'I love you, Baelaf'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
